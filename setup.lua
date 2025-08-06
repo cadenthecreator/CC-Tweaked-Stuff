bc49f207c9b9fea316addba81b3a002f9a492421
CadenCoaster <114967401+cadenthecreator@users.noreply.github.com>
2025-08-05 20:40:47 -0700

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -8,23 +8,26 @@ end
 term.write("setup netmountcc? (Y/n)")
 
 if isYes(read()) then
-        term.write("url: ")
-        local url = read()
-        term.write("user: ")
-        local user = read()
-        term.write("pass: ")
-        local pass = read("*")
-        if http.checkURL(url) then
-            settings.set("netmount.url",url)
-            settings.set("netmount.username",user)
-            settings.set("netmount.password",pass)
-            settings.set("netmount.path","/cloud")
-            settings.save()
-            fs.makeDir("/startup")
-            local file = fs.open("/startup/-01_mount.lua","w")
-            file.write(http.get(url.."/mount.lua").readAll())
-            file.close()
-        end 
+    term.write("url: ")
+    local url = read()
+    term.write("user: ")
+    local user = read()
+    term.write("pass: ")
+    local pass = read()
+    term.clear()
+    term.setCursorPos(1,1)
+    if http.checkURL(url) then
+        settings.set("netmount.url",url)
+        settings.set("netmount.username",user)
+        settings.set("netmount.password",pass)
+        settings.set("netmount.path","/cloud")
+        settings.save()
+        fs.makeDir("/startup")
+        local file = fs.open("/startup/-01_mount.lua","w")
+        file.write(http.get(url.."/mount.lua").readAll())
+        file.close()
+    end 
+   print("done setting up netmountcc!")     
 end
 
 os.reboot()
